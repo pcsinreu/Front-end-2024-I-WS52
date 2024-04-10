bb3c9d8541f0dd4e74c7e24740d77370309ff326
Naldo Reupo <naldo_reupo-musayon@mckinsey.com>
2024-04-09 20:44:24 -0500

    feat : add ui components

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,27 @@
 </script>
 
 <template>
+ <main>
  <button>Click me!</button>
- <pv-button label="Click me" />
+ <pv-button label="Click me"/>
+ <pv-calendar />
+ <pv-rating />
+  <pv-input-text />
+
+
+    <pv-input-mask id="basic"  mask="99-999999" placeholder="99-999999" />
+
+
+   <pv-input-mask id="basic"  mask="aaaaaaa" placeholder="solo letras" />
+
+
+
+
+ </main>
 </template>
 
 <style scoped>
+main{
+ background-color:red;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from "primevue/config";
-import 'primevue/resources/themes/lara-dark-pink/theme.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
 import Button from "primevue/button";
+import Calendar from 'primevue/calendar';
+import Rating from 'primevue/rating';
+import InputText from 'primevue/inputtext';
+import InputMask from 'primevue/inputmask'
+
+
+
 
 const app = createApp(App);
 app.use(PrimeVue); // inyección de depencias
 app.component('pv-button',Button)
+app.component('pv-calendar',Calendar)
+app.component('pv-rating',Rating)
+app.component('pv-input-text',InputText)
+app.component('pv-input-mask',InputMask)
 
 app.mount('#app')
